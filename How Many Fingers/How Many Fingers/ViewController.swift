ec75d1e71288073ded4fb3868f6f7b25b9228419
Hoang Dang <hoangdang1449@gmail.com>
2016-03-16 22:38:13 +0700

    While loops review

---
+++ How Many Fingers/How Many Fingers/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  How Many Fingers
+//
+//  Created by Hoang Dang on 3/15/16.
+//  Copyright © 2016 Hoang Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var userGuessTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func guess(sender: AnyObject) {
+        
+        
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ While loops.playground/Contents.swift
@@ -0,0 +1,51 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var i = 1
+
+while i < 100 {
+    
+    print(i)
+    
+    i = i * 2
+}
+
+var i1 = 10
+
+while i1 < 100 {
+    
+    print(i1)
+    
+    i1 = i1 + 10
+}
+
+var i2 = 1
+
+while i2 <= 10 {
+    
+    print(i2 * 5)
+    
+    i2++
+}
+
+var arr = [8, 3, 4, 5, 6, 3]
+var i4 = 0
+
+while i4 < arr.count {
+    
+    print(arr[i4])
+    
+    i4++
+    
+}
+
+var myArr: [Double] = [1, 3, 5, 5, 6, 7]
+var i5 = 0
+
+while i5 < myArr.count {
+    
+    print(myArr[i5] - 1)
+    i5++
+    
+}
